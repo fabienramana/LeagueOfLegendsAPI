459253ee8811c8e471aac3bec6d45db537b82779
Fabien <fabien.rmnd@gmail.com>
2019-07-17 17:26:36 +0200

    add request service

--- src/modules/basics/router.js
+++ src/modules/basics/router.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
-const config = require('config');
-const request = require('request');
+const { getRoute } = require('../../services/request');
 
 const router = new Router();
 
@@ -13,14 +12,17 @@ router.use('/ping', (req, res) => {
 });
 
 router.use('/lol', (req, res) => {
-  request({
-    uri: `https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=${config.get('api_key')}`,
-    method: 'GET',
-    timeout: 10000,
-  }, (error, response, body) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(body);
-  });
+  getRoute('https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations')
+    .then((body) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(body);
+    })
+    .catch((err) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(
+        err,
+      );
+    });
 });
 
 module.exports = router;
---
+++ src/services/request.js
@@ -0,0 +1,25 @@
+const config = require('config');
+const request = require('request');
+
+function getRoute(url) {
+  return new Promise(((resolve, reject) => {
+    request({
+      uri: url,
+      method: 'GET',
+      timeout: 10000,
+      headers: {
+        'X-Riot-Token': config.get('api_key'),
+      },
+    }, (error, response, body) => {
+      const responseJson = JSON.parse(body);
+      if (error) {
+        return reject(error);
+      }
+      return resolve(responseJson);
+    });
+  }));
+}
+
+module.exports = {
+  getRoute,
+};
