42323a3a0402124d1ea167023b3d8025907d18d5
Fabien <fabien.rmnd@gmail.com>
2019-07-17 17:54:46 +0200

    add first connction to LoL api

--- config/default.js
+++ config/default.js
@@ -1,5 +1,6 @@
 module.exports = {
   port: 3000,
+  api_key: 'RGAPI-358b52b7-8396-4b7d-ba9a-bda8bb65c658',
   database: {
     url: 'mongodb://localhost:27017/todolist',
   },
--- src/modules/basics/router.js
+++ src/modules/basics/router.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const config = require('config');
+const request = require('request');
 
 const router = new Router();
 
@@ -7,6 +9,16 @@ const router = new Router();
 router.use('/ping', (req, res) => {
   res.send({
     ping: 'pong',
+  });
+});
+
+router.use('/lol', (req, res) => {
+  request({
+    uri: `https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=${config.get('api_key')}`,
+    method: 'GET',
+    timeout: 10000,
+  }, (error, response, body) => {
+    res.send(body);
   });
 });
 
