e7f6dd2aa83392f13d4120d4869358c5f2353c60
Fabien <fabien.rmnd@gmail.com>
2019-09-11 09:36:56 +0200

    Revert "Mohammed commits"
    This reverts commit 90e5d3a385b53de6f18bfb48adab8cf0f000ae63.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('config');
-const passport = require('passport');
 const apiRouter = require('./services/api');
 const errors = require('./middleware/errors');
 const notFound = require('./middleware/notFound');
@@ -16,10 +15,6 @@ server.use(apiRouter);
 
 // Last one
 server.use(notFound);
-// passport Init
-
-server.use(passport.initialize());
-server.use(passport.session());
 
 // Middleware d'erreur
 server.use(errors);
